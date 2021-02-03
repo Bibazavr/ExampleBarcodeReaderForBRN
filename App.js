3374e1c0f28e31e65a4e173db7ec9796fa89cd7a
Bibazavr <lirik.hariev@gmail.com>
2021-02-03 12:05:23 +0300

    onBarCodeScanned works

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  Alert,
+} from 'react-native';
 import {Camera} from 'expo-camera';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+});
+
+const Response = (props) => {
+  return (
+    <View>
+      <Text>Последние полученные данные:</Text>
+      <Text>{JSON.stringify(props.response)}</Text>
+    </View>
+  );
+};
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.torch);
+  const [response, setResponse] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -20,8 +66,16 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Шикарная библиотека expo-camera</Text>
+      <Camera
+        style={styles.camera}
+        type={type}
+        flashMode={flash}
+        onBarCodeScanned={(resp) => {
+          Alert.alert('Прочитаны данные:', JSON.stringify(resp));
+          setResponse(resp);
+        }}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.button}
@@ -32,34 +86,22 @@ export default function App() {
                   : Camera.Constants.Type.back,
               );
             }}>
-            <Text style={styles.text}> Flip </Text>
+            <Text style={styles.text}> Flip camera </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setFlash(
+                flash === Camera.Constants.FlashMode.torch
+                  ? Camera.Constants.FlashMode.off
+                  : Camera.Constants.FlashMode.torch,
+              );
+            }}>
+            <Text style={styles.text}> Flash </Text>
           </TouchableOpacity>
         </View>
       </Camera>
-    </View>
+      <Response response={response} />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
-});
